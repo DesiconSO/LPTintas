1ff9b2115b61aef788c9ce2cb64c48a7bae24be3
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-12-21 10:07:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import './App.css'
+import Product from './components/Product'
+import './index.css'
+
+const products = [
+  {
+    id: 1,
+    name: 'Produto 1',
+    description: 'Descrição do produto 1',
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 2,
+    name: 'Produto 2',
+    description: 'Descrição do produto 1',
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 3,
+    name: 'Produto 3',
+    description: 'Descrição do produto 1',
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 4,
+    name: 'Produto 4',
+    description: 'Descrição do produto 1',
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 5,
+    name: 'Produto 5',
+    description: 'Descrição do produto 1',
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 6,
+    name: 'Produto 6',
+    description: 'Descrição do produto 1',
+    image: 'https://picsum.photos/200/300'
+  }
+]
+
+function App() {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <div className="h-2/5 bg-blue-600">
+
+      </div>
+      <div className="h-3/5 flex justify-around items-center">
+        <div className="flex gap-12">
+          {products.map((product) => (
+            <Product product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Product.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import useModal from "../functions/UseModal";
+import Modal from "./Modal";
+
+interface ProductType {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+}
+
+export default function Product({ id, name, description, image }: ProductType) {
+    const { isOpen, toggle } = useModal();
+
+    return (
+        <>
+            <button className="text-center hover:scale-105 duration-150" onClick={toggle}>
+                <div className="w-[180px] h-[180px] bg-white rounded-full shadow-lg"></div>
+                <h3 className='mt-5'>{name}</h3>
+            </button>
+
+            <Modal isOpen={isOpen} toggle={toggle}>
+                {name}
+            </Modal>
+        </>
+    );
+}
